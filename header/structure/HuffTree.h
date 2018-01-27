dd5b07f0ba5ce878a3e19906af4611361312cfb6
Ian Duran <ianduran60@gmail.com>
2018-01-27 09:14:57 -0600

    Method to store the Huffman codes of the values made

--- header/structure/HuffTree.h
+++ header/structure/HuffTree.h
@@ -7,6 +7,9 @@
 
 #include "PriorityQueue.h"
 
+#include <map>
+#include <string>
+#include <utility>
 template <class T>
 struct TreeNode{
     T* value;
@@ -19,12 +22,15 @@ class HuffTree{
 private:
     TreeNode<T>* root;
     int size;
+    void deleteNode(TreeNode<T>*);
+    void storeCodes(TreeNode*, std::string,std::map<std::string, T*>*);
 public:
     explicit HuffTree(T*);
     HuffTree(T*, HuffTree<T>*, HuffTree<T>*);
+    virtual ~HuffTree();
     T* getRootValue();
     TreeNode<T>* getRoot();
-    //void storeCodes();
+    void storeCodes(std::map<std::string, T*>*);
 };
 
 template<class T>
@@ -41,6 +47,24 @@ HuffTree<T>::HuffTree(T* rootValue, HuffTree<T>* firstTree, HuffTree<T>* secondT
 }
 
 template<class T>
+HuffTree<T>::~HuffTree() {
+    this->deleteNode(root);
+}
+
+template<class T>
+void HuffTree<T>::deleteNode(TreeNode<T> *currentNode) {
+    if(currentNode != 0) {
+        if (currentNode->leftChild != 0)
+            this->deleteNode(currentNode->leftChild);
+
+        if (currentNode->rightChild != 0)
+            this->deleteNode(currentNode->rightChild);
+
+        delete currentNode;
+    }
+}
+
+template<class T>
 T* HuffTree<T>::getRootValue() {
     return this->root->value;
 }
@@ -49,6 +73,25 @@ T* HuffTree<T>::getRootValue() {
 template<class T>
 TreeNode<T> *HuffTree<T>::getRoot() {
     return this->root;
+}
+
+template<class T>
+void HuffTree<T>::storeCodes(std::map<std::string, T *> *codeMap) {
+    this->storeCodes(this->root, "",codeMap);
+}
+
+template<class T>
+void HuffTree<T>::storeCodes(TreeNode * currentNode, std::string currentCode, std::map<std::string, T *> *codeMap) {
+    if(currentNode->rightChild == 0 && currentNode->leftChild == 0){
+        codeMap->insert(std::pair<std::string, T*>(currentCode, currentNode->value));
+    }else{
+        if(currentNode->leftChild != 0)
+            this->storeCodes(currentNode->leftChild, currentCode + "0", codeMap);
+
+        if(currentNode->rightChild != 0)
+            this->storeCodes(currentNode->rightChild, currentCode + "1", codeMap);
+
+    }
 }
 
 #endif //COMPRESSOR_HUFFTREE_H
